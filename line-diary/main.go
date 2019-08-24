40d8ca13d81c91749c631f1718f512dab324d733
pomadev <y.pomadev@gmail.com>
2019-08-25 02:51:19 +0900

    Dateをstring型に、日付で日記検索機能追加

--- line-diary/main.go
+++ line-diary/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -17,12 +18,13 @@ import (
 
 type diary struct {
 	LineID string
-	Date   time.Time
+	Date   string
 
 	Content string `dynamo:"Content"`
 }
 
 // TODO: エラー返す処理重複を除く
+// TODO: ネスト地獄解消
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
@@ -72,6 +74,26 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 				}
 			}
 		}
+		if e.Type == linebot.EventTypePostback {
+			if e.Postback.Params.Date != "" {
+				c, err := fetch(e.Source.UserID, e.Postback.Params.Date)
+				if err != nil {
+					if err != dynamo.ErrNotFound {
+						log.Printf("cannot fetch: %v", err)
+						return events.APIGatewayProxyResponse{
+							StatusCode: http.StatusInternalServerError,
+						}, nil
+					}
+					if _, err = bot.ReplyMessage(e.ReplyToken, linebot.NewTextMessage("日記が見つかりませんでした")).Do(); err != nil {
+						log.Printf("bot cannot reply message: %v", err)
+					}
+				} else {
+					if _, err = bot.ReplyMessage(e.ReplyToken, linebot.NewTextMessage(c.Content)).Do(); err != nil {
+						log.Printf("bot cannot reply message: %v", err)
+					}
+				}
+			}
+		}
 	}
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
@@ -87,17 +109,38 @@ func translate(request events.APIGatewayProxyRequest) (*http.Request, error) {
 	return r, nil
 }
 
+// TODO: dynamodb処理処理共通化
 func put(id, content string, t time.Time) error {
 	db := dynamo.New(session.New(), &aws.Config{Region: aws.String(os.Getenv("DYNAMODB_REGION"))})
 	table := db.Table(os.Getenv("DYNAMODB_TABLE"))
 
+	t = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))
+
 	d := diary{
 		LineID:  id,
-		Date:    time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)),
+		Date:    fmt.Sprintf("%d/%d/%d", t.Year(), t.Month(), t.Day()),
 		Content: content,
 	}
 	err := table.Put(d).Run()
 	return err
+}
+
+func fetch(id, s string) (diary, error) {
+	db := dynamo.New(session.New(), &aws.Config{Region: aws.String(os.Getenv("DYNAMODB_REGION"))})
+	table := db.Table(os.Getenv("DYNAMODB_TABLE"))
+
+	// TODO: 処理切り分け
+	layout := "2006-01-02"
+	t, err := time.Parse(layout, s)
+	if err != nil {
+		return diary{}, err
+	}
+
+	var d diary
+	err = table.Get("LineID", id).
+		Range("Date", dynamo.Equal, fmt.Sprintf("%d/%d/%d", t.Year(), t.Month(), t.Day())).
+		One(&d)
+	return d, err
 }
 
 func main() {
